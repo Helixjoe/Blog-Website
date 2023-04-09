20bd7147ad17704690b22edca08d26157642e397
Helixjoe <joeljohn5112@gmail.com>
2023-04-09 21:39:33 +0530

    about and contact routes added

--- app.js
+++ app.js
@@ -18,6 +18,13 @@ app.get("/", (req,res)=>{
   res.render("home",{startingContent: homeStartingContent});
 })
 
+app.get("/about", (req,res)=>{
+  res.render("about",{aboutContent: aboutContent});
+})
+
+app.get("/contact", (req,res)=>{
+  res.render("contact",{contactContent: contactContent});
+})
 
 
 app.listen(3000, function() {
